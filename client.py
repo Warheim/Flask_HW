0302d903c21c3d671ed48c28edb524eef1cb9811
Steshin <warheim24@gmail.com>
2023-02-01 14:11:40 +0300

    UserView written

--- client.py
+++ client.py
@@ -5,7 +5,9 @@ import requests
 #                          json={'email': 'some1@email.com', 'password': '1234Vova!'})
 
 
-response = requests.get('http://127.0.0.1:5000/users/1/')
+# response = requests.get('http://127.0.0.1:5000/users/1/')
+
+response = requests.delete('http://127.0.0.1:5000/users/1/')
 
 print(response.status_code)
 print(response.json())
--- server.py
+++ server.py
@@ -1,29 +1,74 @@
 from flask import Flask, request, jsonify
 from flask.views import MethodView
-from database import Session, UserModel
+from database import Session, UserModel, AdvertisementModel
+from errors import HttpException
+from hashlib import md5
+from sqlalchemy.exc import IntegrityError
 
 app = Flask('app')
+
+
+@app.errorhandler(HttpException)
+def error_handler(error: HttpException):
+    response = jsonify({
+        'status': 'error',
+        'status_code': error.status_code,
+        'message': error.message
+    })
+    return response
+
+
+def get_user(user_id: int, session=Session) -> UserModel:
+    user = session.query(UserModel).get(user_id)
+    if user is None:
+        raise HttpException(
+            status_code=404,
+            message='user not found'
+        )
+    return user
+
+
+def get_adv(adv_id: int, session=Session) -> AdvertisementModel:
+    adv = session.query(AdvertisementModel).get(adv_id)
+    if adv is None:
+        raise HttpException(
+            status_code=404,
+            message='user not found'
+        )
+    return adv
 
 
 class UserOps(MethodView):
 
     def get(self, user_id: int):
         with Session() as session:
-            user = session.query(UserModel).get(user_id)
-        return jsonify({'id': user.id,
-                        'email': user.email,
-                        'password': user.password})
+            user = get_user(user_id, session)
+            return jsonify({
+                'id': user.id,
+                'email': user.email
+            })
 
     def post(self):
         user_data = request.json
+        user_data['password'] = md5(user_data['password'].encode('utf-8').hexdigest())
         with Session() as session:
             new_user = UserModel(**user_data)
             session.add(new_user)
-            session.commit()
-        return jsonify({'status': 'OK'})
+            try:
+                session.commit()
+            except IntegrityError:
+                raise HttpException(
+                    status_code=409,
+                    message='user with this email already exists'
+                )
+            return jsonify({'status': f'new user id is {new_user.id}'})
 
     def delete(self, user_id: int):
-        pass
+        with Session() as session:
+            old_user = get_user(user_id, session)
+            session.delete(old_user)
+            session.commit()
+        return jsonify({'status': f'user {old_user.email} deleted'})
 
 
 class AdvertisementOps(MethodView):
@@ -31,7 +76,7 @@ class AdvertisementOps(MethodView):
     def get(self, adv_id: int):
         pass
 
-    def post(self):
+    def post(self, user_id: int):
         pass
 
     def delete(self, adv_id: int):
@@ -41,7 +86,7 @@ class AdvertisementOps(MethodView):
 app.add_url_rule('/users/<int:user_id>/', view_func=UserOps.as_view('users'), methods=['GET', 'DELETE'])
 app.add_url_rule('/users/', view_func=UserOps.as_view('user_create'), methods=['POST'])
 app.add_url_rule('/adv/<int:adv_id>/', view_func=AdvertisementOps.as_view('advertisements'), methods=['GET', 'DELETE'])
-app.add_url_rule('/adv/', view_func=AdvertisementOps.as_view('advertisements_create'), methods=['POST'])
+app.add_url_rule('/adv/<int:user_id>', view_func=AdvertisementOps.as_view('advertisements_create'), methods=['POST'])
 
 if __name__ == '__main__':
     app.run()
